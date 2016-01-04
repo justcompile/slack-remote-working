9cab99485f676563dd80c58c844408a2bb798ee1
Richard Hayes <rich@justcompile.it>
2016-01-04 01:41:42 +0000

    Added dummy response messages

--- app.js
+++ app.js
@@ -4,8 +4,28 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/locate', function (req, res) {
-  console.log(req.body);
-  res.send('Hello World!');
+  var entityToFind = req.query.text;
+
+  var responseObject = {
+    text: ''
+  };
+
+  if (!entityToFind) {
+    responseObject.text = 'You need to specify a `@username` or use `everyone` in order for me to find who you are looking for...';
+  } else if (entityToFind.toLowerCase() === 'everyone') {
+    var users = ['dave', 'mike', 'sally'];
+
+    var messageLines = [];
+    users.forEach(function(user) {
+      messageLines.push('@'+user+' is working from home today');
+    });
+
+    responseObject.text = messageLines.join('\n');
+  } else {
+    responseObject.text = entityToFind + ' is working in the office today';
+  }
+
+  res.send(responseObject);
 });
 
 var server = app.listen(app.get('port'), function () {
