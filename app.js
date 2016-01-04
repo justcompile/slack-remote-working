3ab309753ab739d9227fd5fe18731a2dcd106fb0
Richard Hayes <rich@justcompile.it>
2016-01-04 02:03:17 +0000

    simple integration with dummy data store

--- app.js
+++ app.js
@@ -7,12 +7,19 @@ app.set('port', (process.env.PORT || 5000));
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
-})); 
+}));
 
 var cache = {};
 
 app.post('/set-location', function(req, res) {
-  console.log(req.body);
+  var username = '@'+req.body.user_name;
+  var message = req.body.text;
+
+  cache[username] = {
+    location: message,
+    updated: Date.now()
+  };
+
   res.send('Thanks. Have a great day!');
 });
 
@@ -26,16 +33,21 @@ app.get('/locate', function (req, res) {
   if (!entityToFind) {
     responseObject.text = 'You need to specify a `@username` or use `everyone` in order for me to find who you are looking for...';
   } else if (entityToFind.toLowerCase() === 'everyone') {
-    var users = ['dave', 'mike', 'sally'];
-
     var messageLines = [];
-    users.forEach(function(user) {
-      messageLines.push('@'+user+' is working from home today');
+
+    Object.keys(cache).forEach(function(username) {
+      var user = cache[username];
+      messageLines.push('`'+username + '` is working ' + user.location + ' today (' + user.updated + ')');
     });
 
     responseObject.text = messageLines.join('\n');
   } else {
-    responseObject.text = entityToFind + ' is working in the office today';
+    var user = cache[entityToFind];
+    if (!user) {
+      responseObject.text = 'Could not find any location information for ' + entityToFind;
+    } else {
+      responseObject.text = '`'+username + '` is working ' + user.location + ' today (' + user.updated + ')';
+    }
   }
 
   res.send(responseObject);
