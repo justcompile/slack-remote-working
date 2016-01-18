0c6e54b7841896e39880e4497bf0c084cdf33ed7
Richard Hayes <rich@justcompile.it>
2016-01-18 11:16:16 +0000

    typo

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   botId: 'U07QHB67K',
   officeIp: '81.149.158.25',
-  slackRTMAPIToken: process.ENV.RTMAPITOKEN,
-  slackWebAPIToken: process.ENV.WEBAPITOKEN
+  slackRTMAPIToken: process.env.RTMAPITOKEN,
+  slackWebAPIToken: process.env.WEBAPITOKEN
 };
